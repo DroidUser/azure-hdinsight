a4655efef3825f102a741e1b802eab8ff953d527
DroidUser <kumar.kailash38@gmail.com>
2017-03-28 14:55:30 +0530

    Update spark-install.sh

--- spark-install.sh
+++ spark-install.sh
@@ -115,8 +115,8 @@ _init(){
 	chown -R spark: /var/run/spark2
 
 	#update the master hostname in configuration files
-	sed -i 's/{{namenode-hostnames}}/thrift:\/\/${active_namenode_hostname}:9083,thrift:\/\/${secondary_namenode_hostname}:9083/g' /etc/spark2/$HDP_VERSION/0/hive-site.xml
-	sed -i 's/{{history-server-hostname}}/${active_namenode_hostname}:18080/g' /etc/spark2/$HDP_VERSION/0/spark-defaults.conf
+	sed -i 's|{{namenode-hostnames}}|thrift:\/\/'"${active_namenode_hostname}"':9083,thrift:\/\/'"${secondary_namenode_hostname}"':9083|g' /etc/spark2/$HDP_VERSION/0/hive-site.xml
+	sed -i 's|{{history-server-hostname}}|'"${active_namenode_hostname}"':18080|g' /etc/spark2/$HDP_VERSION/0/spark-defaults.conf
 	
 	long_hostname=`hostname -f`
 	
@@ -131,7 +131,7 @@ _init(){
 	 	eval ./sbin/start-thriftserver.sh
 	else
 		cd /usr/hdp/current/spark2-client
-		eval ./sbin/start-slave.sh
+		eval ./sbin/start-slave.sh -i spark://$active_namenode_hostname:18080
 	fi	 
 
 	hadoop_version=eval hadoop version | head -1
